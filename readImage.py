1b548a7b082a4042a1a7150bf857610cf03f6dd4
Allisterlim <Allisterlim77@gmail.com>
2023-12-12 17:59:54 +1100

    separated draw and ocr into 2 functions

--- readImage.py
+++ readImage.py
@@ -5,56 +5,68 @@ import matplotlib.pyplot as plt
 import json
 
 
-pytesseract.pytesseract.tesseract_cmd = (
-    # r"c:\Program Files\Tesseract-OCR\tesseract.exe" # Nyan's one
-    r"c:\Users\allis\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
-)
+def draw_on_image():
+    img = plt.imread("screenshot_0.jpg")
+    fig, ax = plt.subplots()
 
-img = plt.imread("screenshot_0.jpg")
-fig, ax = plt.subplots()
+    with open("screenshot_0.json") as f:
+        screenshot_data = json.load(f)
+        print(screenshot_data)
 
-with open("screenshot_0.json") as f:
-    screenshot_data = json.load(f)
-    print(screenshot_data)
+    ax.imshow(img)
+    x = screenshot_data["gaze_x"]
+    y = screenshot_data["gaze_y"]
 
-ax.imshow(img)
-x = screenshot_data["gaze_x"]
-y = screenshot_data["gaze_y"]
-
-
-ax.scatter(x, y)
-plt.savefig("screenshot_with_draw_0")
-
-# plt.show()
-
-# write code to draw a box at the different points, this is where you're going to OCR.
-# define width and height of this box
-crop_width, crop_height = 300, 200
-
-timesteps = screenshot_data["time"]
-
-# at each time step, crop the image at the x,y coordinates and save
-for timestep in timesteps:
-    image_x = screenshot_data["gaze_x"][timestep]
-    image_y = screenshot_data["gaze_y"][timestep]
-
-    # Calculate the top-left corner coordinates
-    start_x = image_x - crop_width // 2
-    start_y = image_y - crop_height // 2
-
-    cropped_image = img[start_y : start_y + crop_height, start_x : start_x + crop_width]
-
-    # Convert the cropped image to a PIL Image
-    cropped_image_pil = Image.fromarray(cropped_image)
-
-    # Save the cropped image
-    cropped_image_pil.save(f"cropped_image_{timestep}.jpg")
-
-    crop_text = pytesseract.image_to_string(cropped_image)
-
-    print("timestep:", timestep)
-    print("crop_text:", crop_text)
+    ax.scatter(x, y)
+    plt.savefig("screenshot_with_draw_0")
+    # plt.show()
 
 
-# draw this to visualize and adjust how big your OCR boxes should be
-# you might be unnecessarily converting to numpy arrays and then back to images.
+def read_image():
+    with open("screenshot_0.json") as f:
+        screenshot_data = json.load(f)
+        print(screenshot_data)
+
+    screenshot = Image.open("screenshot_0.jpg")
+
+    # write code to draw a box at the different points, this is where you're going to OCR.
+    # define width and height of this box
+    crop_width, crop_height = 300, 200
+
+    timesteps = screenshot_data["time"]
+
+    # at each time step, crop the image at the x,y coordinates and save
+    for timestep in timesteps:
+        image_x = screenshot_data["gaze_x"][timestep]
+        image_y = screenshot_data["gaze_y"][timestep]
+
+        # Calculate the top-left corner of the cropping box
+        start_x = image_x - crop_width // 2
+        start_y = image_y - crop_height // 2
+
+        # Crop the image
+        cropped_image = screenshot.crop(
+            (start_x, start_y, start_x + crop_width, start_y + crop_height)
+        )
+
+        # save image
+        # cropped_image.save(f"cropped_image_{timestep}.jpg")
+
+        crop_text = pytesseract.image_to_string(cropped_image)
+
+        print("timestep:", timestep)
+        print("crop_text:", crop_text)
+
+
+if __name__ == "__main__":
+    pytesseract.pytesseract.tesseract_cmd = (
+        # r"c:\Program Files\Tesseract-OCR\tesseract.exe" # Nyan's one
+        r"c:\Users\allis\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+    )
+
+    # draw_on_image()
+
+    read_image()
+
+    # draw this to visualize and adjust how big your OCR boxes should be
+    # you might be unnecessarily converting to numpy arrays and then back to images.
